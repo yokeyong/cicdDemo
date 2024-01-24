712cf5259b5105a00d4265afb429eb4a7b267a9b
yokeyong <yokeyong.wong@gmail.com>
2024-01-24 19:28:06 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/cicdDemo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicdDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
